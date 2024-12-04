db42b26f010e4dee8e6346dbed15b2cecd43d57c
tbeaumont79 <tbeaumont79@icloud.com>
2024-12-04 12:52:23 +0100

    Feat: adding color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E91E63",
+        accent: "#00BCD4",
+        lighttext: "#B0BEC5",
+      },
+      background: {
+        primary: "#121212",
+        secondary: "#6A1B9A",
+      },
+    },
   },
   plugins: [],
-}
-
+};
