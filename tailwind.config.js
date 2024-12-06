a8e98be1fc1c1ac8604b2eb53e46e04f77a63cd5
tbeaumont79 <tbeaumont79@icloud.com>
2024-12-06 16:00:23 +0100

    Feat: creating the hero component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -11,6 +13,19 @@ module.exports = {
       background: {
         primary: "#121212",
         secondary: "#6A1B9A",
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.066rem",
+        "2xl": "1.421rem",
+        "3xl": "1.894rem",
+        "4xl": "2.525rem",
+        "5xl": "3.366rem",
+      },
+      fontFamily: {
+        roboto: ["Roboto", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
     },
   },
