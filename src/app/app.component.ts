11d79bd2a112758844e3c3dd894c98f9f1d038f8
tbeaumont79 <tbeaumont79@icloud.com>
2025-01-25 21:25:05 +0100

    fix: header was fix inside the hero section instead of the entire app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
-import { AboutSectionComponent } from "./components/about-section/about-section.component";
-import { ProjectSectionComponent } from "./components/project-section/project-section.component";
-
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { ProjectSectionComponent } from './components/project-section/project-section.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroSectionComponent, AboutSectionComponent, ProjectSectionComponent],
+  imports: [
+    NavbarComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    ProjectSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
