50953a58fe34c23024dcffa81973ac14cb301b30
tbeaumont79 <tbeaumont79@icloud.com>
2025-01-25 15:48:09 +0100

    feat: adding alt to social icons in hero section

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -13,11 +13,17 @@ export class HeroSectionComponent {
     {
       img: '/icons/linkedin.svg',
       link: 'https://www.linkedin.com/in/beaumont-thibault/',
+      alt: 'linkedin',
     },
-    { img: '/icons/github.svg', link: 'https://github.com/Tbeaumont79' },
+    {
+      img: '/icons/github.svg',
+      link: 'https://github.com/Tbeaumont79',
+      alt: 'github',
+    },
     {
       img: '/icons/bluesky.svg',
       link: 'https://bsky.app/profile/thibaultb79.bsky.social',
+      alt: 'bluesky',
     },
   ];
   toggle() {
