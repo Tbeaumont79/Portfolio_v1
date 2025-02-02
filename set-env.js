171b3fb72ba74791ecd41e5b40d309619cf99e0f
tbeaumont79 <tbeaumont79@icloud.com>
2025-02-02 08:46:33 +0100

    feat: ready for prod

---
+++ set-env.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+// targetPath should reflect where the .env file should go
+const targetPath = "./src/environments/environment.prod.ts";
+const envConfigFile = `
+export const environment = {
+  production: true,
+  // name should match Netlify env key/keys
+  supabaseURL: '${process.env.supabaseURL}',
+  supabaseKey: '${process.env.supabaseKey}'
+};
+`;
+fs.writeFileSync(targetPath, envConfigFile);
+console.log(`Output generated at ${targetPath}`);
