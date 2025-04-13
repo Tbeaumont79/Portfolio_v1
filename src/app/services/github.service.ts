4f96e2f7a2fd373376b4942c54bc2aec855af886
tbeaumont79 <tbeaumont79@icloud.com>
2025-04-13 11:05:54 +0200

    fix: removing unnecessary http header to get all repo

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,10 +11,6 @@ export class GithubService {
   constructor(private http: HttpClient) {}
 
   getRepos(): Observable<any> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${environment.apiKey}`,
-    });
-
-    return this.http.get(this.apiUrl, { headers });
+    return this.http.get(this.apiUrl);
   }
 }
