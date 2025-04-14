37244bbb0efb1790e5bddc777b5b60550e75bc0c
tbeaumont79 <tbeaumont79@icloud.com>
2025-04-14 21:31:59 +0200

    feat: update footer social links

--- src/app/components/blog-list-post/blog-list-post.component.ts
+++ src/app/components/blog-list-post/blog-list-post.component.ts
@@ -16,23 +16,26 @@ export class BlogListPostComponent {
         "I've always been passioned about web development and now I'm starting a career in it, this is my first blog post and i will tell you everything i went through to get here and what i'm looking forward to.",
     },
     {
+      slug: 'web-accessibility-post',
+      title: 'Enhancing Web Accessibility Best Practices for Developers',
+      date: 'Feb 1, 2025',
+      description:
+        'A guide to improving web accessibility with practical tips, examples, and best practices for developers.',
+    },
+    {
       slug: 'myjourney-mercureandsymfony-post',
-      title: 'Building a Real-Time Chat App, my Journey with Mercure and Symfony',
+      title:
+        'Building a Real-Time Chat App, my Journey with Mercure and Symfony',
       date: 'Feb 23, 2025',
-      description: "I've created a chat app using symfony and mercure, in this blog post i will tell you how to setup mercure with docker and symfony.",
+      description:
+        "I've created a chat app using symfony and mercure, in this blog post i will tell you how to setup mercure with docker and symfony.",
     },
     {
       slug: 'php-vite-post',
       title: 'How to use Vite with PHP',
       date: 'Apr 8, 2025',
-      description: "In this posts we are going to take a look at how to use Vite with PHP. Why it can be usefull and how to configure it.",
+      description:
+        'In this posts we are going to take a look at how to use Vite with PHP. Why it can be usefull and how to configure it.',
     },
-    {
-      slug: 'web-accessibility-post',
-      title: 'Enhancing Web Accessibility Best Practices for Developers',
-      date: 'Feb 1, 2025',
-      description: "A guide to improving web accessibility with practical tips, examples, and best practices for developers.",
-      
-    }
   ];
 }
