97aa3e25881b6ad2a6497392b4878699cd4c1f91
tbeaumont79 <tbeaumont79@icloud.com>
2025-04-15 15:29:22 +0200

    feat(hero-section): adding hover color on svg

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,29 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-hero-section',
-    imports: [],
-    templateUrl: './hero-section.component.html'
+  selector: 'app-hero-section',
+  imports: [],
+  templateUrl: './hero-section.component.html',
 })
 export class HeroSectionComponent {
   isOpen = false;
-  socialLinks = [
-    {
-      img: '/icons/linkedin.svg',
-      link: 'https://www.linkedin.com/in/beaumont-thibault/',
-      alt: 'linkedin',
-    },
-    {
-      img: '/icons/github.svg',
-      link: 'https://github.com/Tbeaumont79',
-      alt: 'github',
-    },
-    {
-      img: '/icons/bluesky.svg',
-      link: 'https://bsky.app/profile/thibaultb79.bsky.social',
-      alt: 'bluesky',
-    },
-  ];
+
   toggle() {
     this.isOpen = !this.isOpen;
   }
