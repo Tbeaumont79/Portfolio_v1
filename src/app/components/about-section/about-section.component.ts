f0cf87db80095144de63b9ab5e0d9c527883b0a2
tbeaumont79 <tbeaumont79@icloud.com>
2025-04-24 10:01:38 +0200

    fix(asset): in about component for prod

--- src/app/components/about-section/about-section.component.ts
+++ src/app/components/about-section/about-section.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about-section',
-    imports: [],
-    templateUrl: './about-section.component.html'
+  selector: 'app-about-section',
+  imports: [],
+  templateUrl: './about-section.component.html',
 })
 export class AboutSectionComponent {
   aboutInfo = [
-    { img: '/icons/mapPoint.svg', text: 'Paris, France', alt:'icon de localisation' },
-    { img: '/icons/phone.svg', text: '06.51.51.16.19', alt:'icon de telephone' },
-    { img: '/icons/letter.svg', text: 'beaumont.thibault@icloud.com', alt:'icon de mail' },
+    {
+      img: 'icons/mapPoint.svg',
+      text: 'Paris, France',
+      alt: 'icon de localisation',
+    },
+    {
+      img: 'icons/phone.svg',
+      text: '06.51.51.16.19',
+      alt: 'icon de telephone',
+    },
+    {
+      img: 'icons/letter.svg',
+      text: 'beaumont.thibault@icloud.com',
+      alt: 'icon de mail',
+    },
   ];
 }
